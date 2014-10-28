0ed6a1f13ad80af50d0e3bee10117ad8a32c185a
George Nishimura <thetmkay@gmail.com>
2014-10-28 15:17:01 +0000

    added scoped scss file structure and fix js

--- js/header.js
+++ js/header.js
@@ -1,8 +1,12 @@
- (function($) {
-     $(document).ready(function() {
-         $('.nav-menu-trigger').click(function(event) {
-             event.preventDefault();
-             $('.header').toggleClass('open-menu');
-         });
-     });
- })(jQuery);
+(function($) {
+
+    'use strict';
+
+    $(document).ready(function() {
+        $('.nav-menu-trigger').click(function(event) {
+            event.preventDefault();
+            $('.header').toggleClass('open-menu');
+        })
+    })
+
+})(jQuery);
